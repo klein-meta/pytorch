018b965bf6a04a2e727dbdef19c38454023e3ce2
leslie-fang-intel <leslie.fang@intel.com>
2024-05-19 20:10:20 -0700

    Unify the dtype to VecMask<float, N> in ops.masked
    ghstack-source-id: 8f5dfad886911b5dd38511d18118ff58760700ea
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/126662

--- torch/_inductor/codegen/cpp.py
+++ torch/_inductor/codegen/cpp.py
@@ -1419,6 +1419,12 @@ class CppVecOverrides(CppOverrides):
             code = BracesBuffer()
             code.writeline("[&]")
             with V.kernel.swap_buffers(code), code.indent():
+                if dtype == torch.bool and result.is_vec:
+                    # Unify blendv to use the VecMask<float, N>, since loading bool as VecMask<float> by default
+                    dtype = torch.float
+                    num_vectors = V.kernel._get_num_vectors(dtype)
+                    other_code_vec = f"at::vec::VecMask<{DTYPE_TO_CPP[dtype]}, {num_vectors}>::from({other_code})"
+
                 code.writeline(f"if ({new_mask}.all_zero())")
                 with code.indent():
                     code.writeline(f"return {other_code_vec};")
